d814124725107fd2bde1a3ca2aa8af090606c55e
Veneta Miteva <79469742+VenetaMiteva@users.noreply.github.com>
2021-02-23 22:18:11 +0200

    Update SQL_queries.sql

--- SQL queries/SQL_queries.sql
+++ SQL queries/SQL_queries.sql
@@ -6,13 +6,13 @@ FROM users;
 -- 2. Най-старият потребител.
 SELECT * FROM users
 ORDER BY birthDate ASC
-LIMIT 1
+LIMIT 1;
 
 
 -- 3. Най-младият потребител.
 SELECT * FROM users
 ORDER BY birthDate DESC
-LIMIT 1
+LIMIT 1;
 
 
 -- 4. Колко юзъра са регистрирани с мейли от abv, колко от gmail и колко с различни от двата.
@@ -72,7 +72,7 @@ WHERE LENGTH(TRIM(caption)) = (SELECT LENGTH(TRIM(caption)) FROM posts
 WHERE LENGTH(TRIM(caption)) > 0
 ORDER BY LENGTH(caption) ASC 
 LIMIT 1)
-ORDER BY LENGTH(caption) ASC
+ORDER BY LENGTH(caption) ASC;
 
 
 -- 14. Покажете поста с най-дълъг caption.
@@ -80,7 +80,7 @@ SELECT * FROM posts
 WHERE LENGTH(caption) = (SELECT LENGTH(caption) FROM posts
 ORDER BY LENGTH(caption) DESC 
 LIMIT 1)
-ORDER BY LENGTH(caption) DESC
+ORDER BY LENGTH(caption) DESC;
 
 
 -- 15. Кой потребител има най-много постове. Използвайте join заявка.
@@ -146,7 +146,7 @@ ORDER BY cnt_posts DESC;
 -- 19. Кои са постовете с най-много коментари. Използвайте вложена заявка и where clause.
 SELECT posts.* FROM posts
 WHERE posts.id IN (SELECT postId FROM comments)
-ORDER BY posts.commentsCount DESC
+ORDER BY posts.commentsCount DESC;
 
 
 -- 20. Покажете най-стария пост. Може да използвате order или aggregate function.
@@ -166,7 +166,7 @@ SELECT * FROM posts
 WHERE LENGTH(caption) = (SELECT LENGTH(caption) FROM posts
 ORDER BY LENGTH(caption) ASC 
 LIMIT 1)
-ORDER BY LENGTH(caption) ASC
+ORDER BY LENGTH(caption) ASC;
 
 
 -- 23. Създайте потребител през UI-а, добавете му public пост през базата и проверете дали се е създал през UI-а.
@@ -180,7 +180,7 @@ VALUES ('Time stops', 'https://cdn.cdnparenting.com/articles/2020/04/04145119/34
 SELECT posts.id, posts.caption, posts.coverUrl, comments.content
 FROM posts
 LEFT JOIN comments
-ON comments.userId = posts.userId
+ON comments.userId = posts.userId;
 
 
 -- 25. Покажете само постове с коментари и самите коментари.
@@ -188,7 +188,7 @@ SELECT posts.id, posts.caption, posts.coverUrl, comments.content
 FROM posts
 LEFT JOIN comments
 ON comments.userId = posts.userId
-WHERE content IS NOT NULL AND TRIM(content) != '' 
+WHERE content IS NOT NULL AND TRIM(content) != '';
 
 
 -- 26. Покажете името на потребителя с най-много коментари. Използвайте join клауза.
@@ -200,7 +200,7 @@ WHERE userId = (SELECT userId FROM comments
 GROUP BY userId
 ORDER BY COUNT(*) DESC
 LIMIT 1)
-GROUP BY userId
+GROUP BY userId;
 
 
 -- 27. Покажете всички коментари, към кой пост принадлежат и кой ги е направил. Използвайте join клауза.
@@ -209,27 +209,27 @@ FROM comments
 LEFT JOIN posts
 ON comments.postId = posts.userId
 LEFT JOIN users
-ON postId = users.id
+ON postId = users.id;
 
 
 -- 28. Кои потребители са like-нали най-много постове.
 SELECT usersId, COUNT(postsId) AS cnt FROM users_liked_posts
 GROUP BY usersId
-ORDER BY cnt DESC
+ORDER BY cnt DESC;
 
 
 -- 29. Кои потребители не са like-вали постове.
 SELECT users.* FROM users
 LEFT JOIN users_liked_posts 
 ON users.id = users_liked_posts.usersId
-WHERE users.id NOT IN (SELECT usersId FROM users_liked_posts)
+WHERE users.id NOT IN (SELECT usersId FROM users_liked_posts);
 
 
 -- 30. Кои постове имат like-ове. Покажете id на поста и caption.
 SELECT users_liked_posts.postsId, posts.caption
 FROM users_liked_posts
 LEFT JOIN posts
-ON postsId = posts.id
+ON postsId = posts.id;
 
 
 -- 31. Кои постове имат най-много like-ове. Покажете id на поста и caption.
@@ -238,23 +238,23 @@ FROM users_liked_posts
 LEFT JOIN posts
 ON postsId = posts.id
 GROUP BY postsId
-ORDER BY cnt DESC
+ORDER BY cnt DESC;
 
 
 -- 32. Покажете всички потребители, които не follow-ват никого.
 SELECT users.id, users.username FROM users
 LEFT JOIN users_followers_users 
 ON users.id = users_followers_users.usersId_2
-WHERE users.id NOT IN (SELECT usersId_2 FROM users_followers_users)
+WHERE users.id NOT IN (SELECT usersId_2 FROM users_followers_users);
 
 
 -- 33. Покажете всички потребители, които не са follow-нати от никого.
 SELECT users.id, users.username FROM users
 LEFT JOIN users_followers_users 
 ON users.id = users_followers_users.usersId_1
-WHERE users.id NOT IN (SELECT usersId_1 FROM users_followers_users)
+WHERE users.id NOT IN (SELECT usersId_1 FROM users_followers_users);
 
 
 -- 34. Регистрирайте потребител през UI. Follow-нете някой съществуващ потребител и проверете дали записа го има в базата.
 SELECT * FROM users_followers_users
-WHERE usersId_2 = (SELECT id FROM users WHERE email = 'test21@test.test')
+WHERE usersId_2 = (SELECT id FROM users WHERE email = 'test21@test.test');
